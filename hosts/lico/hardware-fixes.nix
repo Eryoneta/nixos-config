4adece4e270f47b3ff567aa5df7b8088d2cc0b40
Eryoneta <eryoneta@gmail.com>
2024-07-08 16:21:37 -0300

    Ajuste de Harware-Fix
    - (ALT) 'lico/harware-fixes.nix': Ajustado fix de bootloader
    . Adicionado comentário e duplicado fix(Faz o terminal 'Stage 1' iniciar um pouco mais abaixo, não mais ocultando o logo 'NixOS')

--- hosts/lico/hardware-fixes.nix
+++ hosts/lico/hardware-fixes.nix
@@ -5,7 +5,7 @@
     boot.loader.grub.extraInstallCommands = ''
       TEMP_FILE="/boot/grub/grubTEMP.tmp"
       GRUB_FILE="/boot/grub/grub.cfg"
-      ${pkgs.coreutils}/bin/echo "videoinfo" | ${pkgs.coreutils}/bin/cat - $GRUB_FILE > $TEMP_FILE && ${pkgs.coreutils}/bin/mv $TEMP_FILE $GRUB_FILE
+      ${pkgs.coreutils}/bin/printf "# Hardware-fix: Atrasa Grub para poder carregar o display\nvideoinfo\nvideoinfo\n\n" | ${pkgs.coreutils}/bin/cat - $GRUB_FILE > $TEMP_FILE && ${pkgs.coreutils}/bin/mv $TEMP_FILE $GRUB_FILE
     '';
 
     # Dead touchpad
