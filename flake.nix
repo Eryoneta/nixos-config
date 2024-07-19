f03b78419ae242318b5e215a9af766c240c71fdd
Eryoneta <eryoneta@gmail.com>
2024-07-19 14:39:42 -0300

    Flake: Modularização
    - (ALT) 'flake.nix': Separado funções em 'flake-helper.nix'

--- flake.nix
+++ flake.nix
@@ -25,169 +25,79 @@
 
   # Outputs
   outputs = { self, ... }@extraArgs: (
-    # let 
-    #   ...packages...
-    # in (
-    #   let
-    #     ...defaults & builders...
-    #   in (
-    #     let
-    #       ...hosts & users...
-    #     in (
-    #       ...configurations...
-    #     )
-    #   )
-    # )
     let
 
-      # Sistem
+      # Packages
       nixpkgs = extraArgs.nixpkgs;
       home-manager = extraArgs.home-manager;
-
-      # Packages
-      pkgsConfig = host: {
-        system = host.system.architecture;
-        config.allowUnfree = true;
+      pgks-bundle = {
+        stable = extraArgs.nixpkgs-stable;
+        unstable = extraArgs.nixpkgs-unstable;
+        unstable-fixed = extraArgs.nixpkgs-unstable-fixed;
       };
-      buildPkgsBundle = host: {
-        stable = (import extraArgs.nixpkgs-stable (pkgsConfig host));
-        unstable = (import extraArgs.nixpkgs-unstable (pkgsConfig host));
-        unstable-fixed = (import extraArgs.nixpkgs-unstable-fixed (pkgsConfig host));
+
+      # Flake Helper
+      helper = import ./flake-helper.nix;
+
+      # Hosts
+      LiCo = helper.buildHost {
+        hostname = "lico";
+        name = "LiCo";
+        system.label = "Flake_Reorder"; #[a-zA-Z0-9:_.-]*
+      };
+      NeLiCo = helper.buildHost {
+        hostname = "nelico";
+        name = "NeLiCo";
+        system.label = ""; #[a-zA-Z0-9:_.-]*
+      };
+
+      # Users
+      Yo = helper.buildUser {
+        username = "yo";
+        name = "Yo";
+        configFolder = "/home/yo/Utilities/SystemConfig/nixos-config";
       };
       
-    in (
-      let
-
-        # Default Host
-        defaultHost = {
-          hostname = "nixos";
-          name = "nixos";
-          user = defaultUser;
-          system = {
-            architecture = "x86_64-linux";
-            label = "";
+      # Common Config
+      commonSystemConfig = user: host: {
+        inherit user;
+        inherit host;
+        nixosConfig = {
+          package = nixpkgs;
+          homeManagerConfig = {
+            enable = true;
+            package = home-manager;
+            extraSpecialArgs.pgks-bundle = pgks-bundle;
           };
-          configFolder = "/etc/nixos";
+          specialArgs.pgks-bundle = pgks-bundle;
         };
-
-        # Default User
-        defaultUser = {
-          username = "nixos";
-          name = "nixos";
-          host = defaultHost;
-          dotFolder = "";
-          configFolder = defaultHost.configFolder;
+      };
+      commonHMConfig = user: host: {
+        inherit user;
+        inherit host;
+        homeManagerConfig = {
+          package = home-manager;
+          systemPackage = nixpkgs;
+          extraSpecialArgs.pgks-bundle = pgks-bundle;
         };
+      };
 
-        # System Builder
-        buildSystem = pair: nixpkgs.lib.nixosSystem {
-          system = pair.host.system.architecture;
-          modules = [
-            (./. + "/hosts/${pair.host.hostname}/configuration.nix")
-            home-manager.nixosModules.home-manager {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.${pair.user.username} = import (./. + "/users/${pair.user.username}/home.nix");
-                extraSpecialArgs = {
-                  user = pair.user;
-                  pkgs-bundle = (buildPkgsBundle pair.host);
-                };
-              };
-            }
-          ];
-          specialArgs = {
-            host = pair.host;
-            pkgs-bundle = (buildPkgsBundle pair.host);
-          };
-        };
+    in {
 
-        # Home-Manager Builder
-        buildHomeManager = pair: home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.${pair.host.system.architecture};
-          modules = [
-            (./. + "/users/${pair.user.username}/home.nix")
-          ];
-          extraSpecialArgs = {
-            user = pair.user;
-            pkgs-bundle = (buildPkgsBundle pair.host);
-          };
-        };
+      # NixOS + Home Manager
+      nixosConfigurations = {
+        "Yo@LiCo" = helper.buildSystem (commonSystemConfig Yo LiCo);
+        #"Yo@NeLiCo" = helper.buildSystem (commonSystemConfig Yo NeLiCo);
+        #"Eryoneta@NeLiCo" = helper.buildSystem (commonSystemConfig Eryoneta NeLiCo);
+      };
+      
+      # Home Manager
+      homeConfigurations = {
+        "Yo@LiCo" = helper.buildHomeManager (commonHMConfig Yo LiCo);
+        #"Yo@NeLiCo" = helper.buildHomeManager (commonHMConfig Yo NeLiCo);
+        #"Eryoneta@NeLiCo" = helper.buildHomeManager (commonHMConfig Eryoneta NeLiCo);
+      };
 
-        # Host Builder
-        buildHost = host: defaultHost // {
-          hostname = host.hostname;
-          name = host.name;
-          system = defaultHost.system // {
-            label = host.system.label;
-          };
-        };
-
-        # User Builder
-        buildUser = user: defaultUser // {
-          username = user.username;
-          name = user.name;
-          configFolder = user.configFolder;
-          dotFolder = buildDotFolderPath user.username user.configFolder;
-        };
-        buildDotFolderPath = username: configFolderPath: "${configFolderPath}/users/${username}/dotfiles";
-
-        # Host-User-Pair Builder
-        buildPair = host: user: {
-          user = user // {
-            host = host // {
-              user = user; # User -> Host -> User -> void
-              configFolder = user.configFolder;
-            };
-          };
-          host = host // {
-            user = user // {
-              host = host; # Host -> User -> Host -> void
-            };
-            configFolder = user.configFolder;
-          };
-        };
-
-      in (
-        let
-
-          # Hosts
-          LiCo = buildHost {
-            hostname = "lico";
-            name = "LiCo";
-            system.label = "Config_Organization:_Default_Reordem"; #[a-zA-Z0-9:_.-]*
-          };
-          NeLiCo = buildHost {
-            hostname = "nelico";
-            name = "NeLiCo";
-            system.label = ""; #[a-zA-Z0-9:_.-]*
-          };
-
-          # Users
-          Yo = buildUser {
-            username = "yo";
-            name = "Yo";
-            configFolder = "/home/yo/Utilities/SystemConfig/nixos-config";
-          };
-
-        in {
-
-          # NixOS + Home Manager
-          nixosConfigurations = {
-            "Yo@LiCo" = buildSystem (buildPair LiCo Yo);
-            #"Yo@NeLiCo" = buildSystem (buildPair NeLiCo Yo);
-            #"Eryoneta@NeLiCo" = buildSystem (buildPair NeLiCo Eryoneta);
-          };
-          
-          # Home Manager
-          homeConfigurations = {
-            "Yo@LiCo" = buildHomeManager (buildPair LiCo Yo);
-            #"Yo@NeLiCo" = buildHomeManager (buildPair NeLiCo Yo);
-            #"Eryoneta@NeLiCo" = buildHomeManager (buildPair NeLiCo Eryoneta);
-          };
-
-        }
-      )
-    )
+    }
   );
 }
