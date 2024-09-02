8538e7fa56700bb2be5fa66427adecc9d8a898b4
Eryoneta <eryoneta@gmail.com>
2024-09-01 23:16:06 -0300

    [ADD] Nix-Modules/MapDir: Comentários
    - (ADD) Adicionado comentários com explicações

--- modules/nix-modules/mapDir.nix
+++ modules/nix-modules/mapDir.nix
@@ -1,5 +1,11 @@
 rec {
+  # mapDir: ./path -> { "file.nix" = ./path/file.nix; subdir = { "file.nix" = ./path/subdir/file.nix }; }
+  #   ./path: A path that points to a folder
+  # Transforms a folder into a series of sets and atributes
+  #   Each set represents a folder, and contains other sets or atributes
+  #   Each atribute represents a file, and contains a path value for it
   mapDir = dirPath: (
+    # mapAttr: { "file.nix" = "regular"; } -> { "file.nix" = ./path/file.nix; }
     builtins.mapAttrs (
       name: value: (
         if (value == "directory") then (
