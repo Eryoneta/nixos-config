83e3607cbdc9e1066fc132870833d84f066b6759
Eryoneta <eryoneta@gmail.com>
2024-09-07 22:19:11 -0300

    [UPD] Programs/Git: Configuração
    - (UPD) Configurado Git

--- public-config/programs/store/home.default.git.nix
+++ public-config/programs/store/home.default.git.nix
@@ -1,13 +1,14 @@
-{ config, pkgs-bundle, user, ... }:
+{ config, pkgs-bundle, user, lib, ... }:
   let
+    mkDefault = value: lib.mkDefault value;
     mkOutOfStoreSymlink = path: config.lib.file.mkOutOfStoreSymlink path;
   in {
     config = {
 
-      # Git: Versionador de arquivos
+      # Git: File Version
       programs.git = {
-        enable = true;
-        package = pkgs-bundle.stable.git; # Pacote: Stable, AutoUpgrade
+        enable = mkDefault true;
+        package = mkDefault pkgs-bundle.stable.git;
       };
 
     };
