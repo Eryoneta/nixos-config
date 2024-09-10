6d1644ab27e43e46d1f30c91af8b002f60cc6890
Eryoneta <eryoneta@gmail.com>
2024-09-10 17:45:52 -0300

    [UPD] Programs/Git: Alias 'loglist'
    - (UPD) Adicionado alias 'loglist'

--- public-config/programs/store/home.default.git.nix
+++ public-config/programs/store/home.default.git.nix
@@ -8,7 +8,31 @@
       programs.git = {
         enable = mkDefault true;
         package = mkDefault pkgs-bundle.stable.git;
+        userName = mkDefault "${user.name}";
+        userEmail = mkDefault "${user.username}@${user.host.hostname}";
+        extraConfig = {
+          init = {
+            defaultBranch = "main";
+          };
+        };
+        includes = [
+          {
+            path = "~/.config/git/aliases/loglist";
+          }
+        ];
       };
+
+      # "git loglist"
+      home.file.".config/git/aliases/loglist".text = ''
+        [alias]
+          # Basically 'git log --graph --oneline', but pretty
+          #   Tip: It accepts '--all'
+          loglist = log \
+            --graph \
+            --abbrev-commit \
+            --decorate \
+            --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset) %C(white)%s%C(reset) %C(yellow)- %an%C(reset)'
+      '';
 
     };
   }
