3f5ad1c871f9a74cba40eb6319be3edbde76060d
Eryoneta <eryoneta@gmail.com>
2024-09-13 14:22:22 -0300

    [ADD] NixOS-Modules: LinkToSourceConfiguration
    - (ADD) Opção de adicionar um symlink para a configuração que gerara a geração
    . Pode ser acessada em '/run/current-system/src'

--- hosts/default/features.nix
+++ hosts/default/features.nix
@@ -1,20 +1,27 @@
-{ host, lib, ... }:
+{ host, lib, modules, ... }:
   let
       mkDefault = value: lib.mkDefault value;
   in {
 
     imports = [
       ./features/auto-upgrade.nix # Auto-Upgrade
+      modules.nixos-modules."link-to-source-config.nix"
     ];
-    
-    # Autologin
-    services.displayManager = {
-      autoLogin.enable = mkDefault false;
-      autoLogin.user = host.user.username;
-    };
 
-    # Source Code
-    system.extraSystemBuilderCmds = "ln -s ${host.configFolderNixStore} $out/src";
-    # "/run/current-system/src" is a link to the current flake source-code!
+    config = {
+      
+      # Auto-login
+      services.displayManager = {
+        autoLogin.enable = mkDefault false;
+        autoLogin.user = host.user.username;
+      };
+
+      # Link to source configuration
+      system.linkToSourceConfiguration = {
+        enable = true;
+        configurationPath = host.configFolderNixStore;
+      };
+
+    };
 
   }
---
+++ modules/nixos-modules/link-to-source-config.nix
@@ -0,0 +1,47 @@
+# Link to Source-Configuration
+# Creates a symlink for the configuration present inside "/nix/store"
+#   Its named "src"
+#   It can be accessed at "/run/current-system/src"
+{ config, lib, ... }:
+  let
+    cfg = config.system.linkToSourceConfiguration;
+  in {
+
+    options = {
+      system.linkToSourceConfiguration = {
+
+        enable = lib.mkEnableOption ''
+          Enables the creation of a symlink for the current configuration at `/run/current-system/src`.
+        '';
+
+        configurationPath = lib.mkOption {
+          type = lib.types.path;
+          description = ''
+            The path for the configuration.
+
+            It can be a path for a user domain ('/home/user/myConfiguration'), but a '/nix/store' path is recommended
+
+            The path for the 'nix/store' can be obtained with `./.` or `self.outPath`.
+          '';
+        };
+
+      };
+    };
+
+    config = lib.mkIf (cfg.enable) {
+
+      assertions = [
+        {
+          assertion = !(cfg.configurationPath == "");
+          message = ''
+            The option 'system.linkToSourceConfiguration' cannot be empty
+          '';
+        }
+      ];
+
+      # Link to source configuration
+      # Available at "/run/current-system/src"
+      system.extraSystemBuilderCmds = "ln -s ${cfg.configurationPath} $out/src";
+
+    };
+  }
