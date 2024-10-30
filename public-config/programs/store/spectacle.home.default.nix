482e25934bc38db8355416373a0e3dff4cd00464
Eryoneta <eryoneta@gmail.com>
2024-10-30 13:40:09 -0300

    [ADD] Programs: Spectacle
    - (ADD) Ferramenta de print

---
+++ public-config/programs/store/spectacle.home.default.nix
@@ -0,0 +1,52 @@
+{ config, pkgs-bundle, ... }@args: with args.config-utils; {
+
+  options = {
+    profile.programs.spectacle = {
+      options.enabled = (utils.mkBoolOption true); # Not used
+      options.packageChannel = (utils.mkPackageOption pkgs-bundle.stable); # Not used
+    };
+  };
+
+  config = with config.profile.programs.spectacle; {
+
+    # Spectacle: Print-screen tool
+    programs.plasma.spectacle = {
+
+      # Shortcuts
+      shortcuts = {
+        launch = (utils.mkDefault) "Print"; # Launch Spectacle
+        launchWithoutCapturing = (utils.mkDefault) "Meta+Print"; # Launch, but do not print
+        captureCurrentMonitor = (utils.mkDefault) "Ctrl+Print"; # Print whole screen
+        captureActiveWindow = (utils.mkDefault) "Alt+Print"; # Print only current focused window
+        captureEntireDesktop = (utils.mkDefault) "Ctrl+Shift+Print"; # Print everything
+        captureRectangularRegion = (utils.mkDefault) "Shift+Print"; # Print a rectangle (Asks where)
+        captureWindowUnderCursor = (utils.mkDefault) ""; # Print the window under cursor
+        recordScreen = (utils.mkDefault) ""; # Record whole screen
+        recordWindow = (utils.mkDefault) ""; # Record only a single window
+      };
+
+    };
+
+    programs.plasma.configFile = {
+
+      # Dotfile
+      "spectaclerc" = {
+        "General" = {
+          "clipboardGroup" = "PostScreenshotCopyImage"; # On print, copy to clipboard
+        };
+        "ImageSave" = {
+          "imageFilenameTemplate" = "img_<yyyy>-<MM>-<dd>_<hh>:<mm>:<ss>";
+          "imageSaveLocation" = "file://${config.xdg.userDirs.pictures}/";
+        };
+        "VideoSave" = {
+          "preferredVideoFormat" = 2; # Save as .mp4
+          "videoFilenameTemplate" = "vid_<yyyy>-<MM>-<dd>_<hh>:<mm>:<ss>";
+          "videoSaveLocation" = "file://${config.xdg.userDirs.videos}/";
+        };
+      };
+
+    };
+
+  };
+
+}
