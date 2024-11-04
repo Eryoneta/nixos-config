eca36eee54374e601e4c0450c81474acf3b1c921
Eryoneta <eryoneta@gmail.com>
2024-11-03 21:13:58 -0300

    [UPD] Programs/(Yakuake, Plasma): Atalhos
    - (UPD) Definido atalhos

--- public-config/programs/desktop-environment-store/plasma-powerdevil.home.default.nix
+++ public-config/programs/desktop-environment-store/plasma-powerdevil.home.default.nix
@@ -89,5 +89,29 @@
 
     };
 
+    # Shortcut
+    programs.plasma.shortcuts = {
+      # Format: "Action = Shortcut,DefaultShortcut,ShortcutTranslatedName"
+      # Actions
+      "org_kde_powerdevil"."Sleep" = "Sleep";
+      "org_kde_powerdevil"."Hibernate" = "Hibernate";
+      "org_kde_powerdevil"."PowerDown" = "Power Down";
+      "org_kde_powerdevil"."PowerOff" = "Power Off";
+      "org_kde_powerdevil"."powerProfile" = [
+        "Meta+B,Battery"
+        "Meta+B,Trocar perfil de energia"
+      ];
+      # Keyboard backlight
+      "org_kde_powerdevil"."Increase Keyboard Brightness" = "Keyboard Brightness Up";
+      "org_kde_powerdevil"."Decrease Keyboard Brightness" = "Keyboard Brightness Down";
+      "org_kde_powerdevil"."Toggle Keyboard Backlight" = "Keyboard Light On/Off";
+      # Screen light
+      "org_kde_powerdevil"."Increase Screen Brightness" = "Monitor Brightness Up";
+      "org_kde_powerdevil"."Increase Screen Brightness Small" = "Shift+Monitor Brightness Up";
+      "org_kde_powerdevil"."Decrease Screen Brightness" = "Monitor Brightness Down";
+      "org_kde_powerdevil"."Decrease Screen Brightness Small" = "Shift+Monitor Brightness Down";
+      "org_kde_powerdevil"."Turn Off Screen" = [ ];
+    };
+
   };
 }
