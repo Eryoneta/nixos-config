e6282128dbf41b0c1b574ed94d2b52fb75085756
Eryoneta <eryoneta@gmail.com>
2024-11-13 15:08:00 -0300

    [UPD] Hosts/LiCo/Fixes: Ajustes
    - (ALT) Melhorado a formatação do fix de 'Grub'
    - (UPD) Desativado touchpad

--- hosts/lico/hardware-fixes.nix
+++ hosts/lico/hardware-fixes.nix
@@ -7,11 +7,18 @@
     boot.loader.grub.extraInstallCommands = ''
       TEMP_FILE="/boot/grub/grubTEMP.tmp"
       GRUB_FILE="/boot/grub/grub.cfg"
-      ${pkgs.coreutils}/bin/printf "# Hardware-fix: Delays Grub to load the display\nvideoinfo\nvideoinfo\n\n" | ${pkgs.coreutils}/bin/cat - $GRUB_FILE > $TEMP_FILE && ${pkgs.coreutils}/bin/mv $TEMP_FILE $GRUB_FILE
+      ${pkgs.coreutils}/bin/printf " \
+        # Hardware-fix: Delays Grub to load the display\n \
+        videoinfo\n \
+        videoinfo\n\n \
+      " | ${pkgs.coreutils}/bin/cat - $GRUB_FILE > $TEMP_FILE \
+        && ${pkgs.coreutils}/bin/mv $TEMP_FILE $GRUB_FILE
     '';
 
     # Dead touchpad
-    #services.libinput.enable = false;
+    services.libinput.touchpad.additionalOptions = ''
+      Option "SendEventsMode" "disabled"
+    '';
     
   };
 }
