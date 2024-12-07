06f0221d46d95c485b54f008939c35e12858423d
Eryoneta <eryoneta@gmail.com>
2024-12-07 02:36:07 -0300

    [UPD] Programs/Plasma/KWin/KZones: Ajustes de zonas
    - (ALT) Dotfiles: Movido kwinscript de 'kwin' para 'kwin/kzones'
    - (UPD) Ajustes nas zonas:
    . Elas agora são geradas, muito mais fáceis de se configurar
    . Os indicadores de zonas são usados para também as selecionar, junto com as bordas da tela

--- public-config/programs/desktop-environment-store/plasma-kwin-script-kzones.home.yo.nix
+++ public-config/programs/desktop-environment-store/plasma-kwin-script-kzones.home.yo.nix
@@ -5,13 +5,14 @@
     home.packages = with options.packageChannel; [
       (kdePackages.kzones.override {
         # Hack incoming: Replace proper package download with my own package in "dotfiles"
-        # It is a newer version
+        # It is a newer, modified version
+        #   (Zones indicators have priority over edges)
         fetchFromGitHub = ignoredArgs: (
-          "${config-domain.public.dotfiles}/kwin"
+          "${config-domain.public.dotfiles}/kwin/kzones"
         );
       })
       #kdePackages.kzones
-      # TODO: (Plasma/KWin/KZones) Remove hack once v0.10 is available
+      # TODO: (Plasma/KWin/KZones) Remove hack once v0.10 is available?
     ];
 
     # Configuration
@@ -27,94 +28,184 @@
         "enableEdgeSnapping" = true; # Snap windows when dragged to edges
         "edgeSnappingTriggerDistance" = 0; # Trigger snap only when at the very edge
         "zoneOverlayIndicatorDisplay" = 1; # Show zone indicators
-        "zoneSelectorTriggerDistance" = 0; # Show zone selector(Top menu) only when at the very top edge
+        "enableZoneSelector" = false; # Disable zone selector(Top menu)
+        #"zoneSelectorTriggerDistance" = 0; # Show zone selector(Top menu) only when at the very top edge
         "layoutsJson" = "[${
           utils.toJSON {
-            "name" = "Quadrant Grid";
-            "padding" = 0;
-            "zones" = [
-              {
-                "x" = 0;
-                "y" = 0;
-                "height" = 50;
-                "width" = 100;
-                "indicator" = {
-                  "position" = "top-center";
+            "name" = "Quadrant Grid"; # Label
+            "padding" = 0; # No space between zones
+            "zones" = (
+              let
+
+                # Utils
+                screen = {
+                  area = { # Area of the screen to be zoned, in %
+                    "x" = 0;
+                    "y" = 0;
+                    "width" = 100;
+                    "height" = 100;
+                  };
+                  hotEdges = [];
                 };
-              }
-              {
-                "x" = 0;
-                "y" = 50;
-                "height" = 50;
-                "width" = 100;
-                "indicator" = {
-                  "position" = "bottom-center";
-                };
-              }
-              {
-                "x" = 0;
-                "y" = 0;
-                "height" = 100;
-                "width" = 100;
-                "indicator" = {
-                  "position" = "center";
-                };
-              }
-              {
-                "x" = 0;
-                "y" = 0.01;
-                "height" = 50;
-                "width" = 50;
-                "indicator" = {
-                  "position" = "top-left";
-                };
-              }
-              {
-                "x" = 50;
-                "y" = 0.01;
-                "height" = 50;
-                "width" = 50;
-                "indicator" = {
-                  "position" = "top-right";
-                };
-              }
-              {
-                "x" = 0;
-                "y" = 50;
-                "height" = 50;
-                "width" = 50;
-                "indicator" = {
-                  "position" = "bottom-left";
-                };
-              }
-              {
-                "x" = 50;
-                "y" = 50;
-                "height" = 50;
-                "width" = 50;
-                "indicator" = {
-                  "position" = "bottom-right";
-                };
-              }
-              {
-                "x" = 0;
-                "y" = 0.01;
-                "height" = 100;
-                "width" = 50;
-                "indicator" = {
-                  "position" = "left-center";
-                };
-              }
-              {
-                "x" = 50;
-                "y" = 0.01;
-                "height" = 100;
-                "width" = 50;
-                "indicator" = {
-                  "position" = "right-center";
-                };
-              }
-            ];
+                tinyVerticalSpace = 0.01; # In %, should be a space so tiny, only the mouse picks it up
+                tinyHorizontalSpace = 0.005; # In %, should be a space so tiny, only the mouse picks it up
+                mkArea = (area: margins: ( # Gets an area and returns it with margins added
+                  let
+                    addVerticalMargin = margin: ( # If the margin required is listed, returns the space to be added
+                      if (builtins.elem margin margins) then tinyVerticalSpace else 0
+                    );
+                    addHorizontalMargin = margin: ( # If the margin required is listed, returns the space to be added
+                      if (builtins.elem margin margins) then tinyHorizontalSpace else 0
+                    );
+                  in {
+                    "x" = area."x" + (addHorizontalMargin "left");
+                    "y" = area."y" + (addVerticalMargin "top");
+                    "width" = area."width" - (addHorizontalMargin "left") - (addHorizontalMargin "right");
+                    "height" = area."height" - (addVerticalMargin "top") - (addVerticalMargin "bottom");
+                  }
+                ));
+                indicatorMargin = -5; # In px, distance to move the zone indicators
+                mkIndicatorMargins = indicatorPosition: ( # Adds a margin to each zone indicator
+                  let
+                    addMargin = position: positions: (
+                      if (builtins.elem position positions) then indicatorMargin else 0
+                    );
+                    margins = {
+                      "center" = [];
+                      "top-left" = [ "top" "left" ];
+                      "top-center" = [ "top" ];
+                      "top-right" = [ "top" "right" ];
+                      "right-center" = [ "right" ];
+                      "bottom-right" = [ "bottom" "right" ];
+                      "bottom-center" = [ "bottom" ];
+                      "bottom-left" = [ "bottom" "left" ];
+                      "left-center" = [ "left" ];
+                    };
+                  in {
+                    "top" = (addMargin "top" margins.${indicatorPosition});
+                    "bottom" = (addMargin "bottom" margins.${indicatorPosition});
+                    "left" = (addMargin "left" margins.${indicatorPosition});
+                    "right" = (addMargin "right" margins.${indicatorPosition});
+                  }
+                );
+                # Originally, indicators are BEHIND hotEdges. Here, moving them doesn't really change anything
+                #   But! My modified "kzones.kwinscript" prioritizes indicators over hotEdges!
+                #   The indicators can be used to easily put windows into corners!
+                mkZone = (area: indicatorPosition: { # Create a kzone
+                  "x" = area."x";
+                  "y" = area."y";
+                  "width" = area."width";
+                  "height" = area."height";
+                  "indicator" = {
+                    "position" = indicatorPosition;
+                    "margin" = (mkIndicatorMargins indicatorPosition);
+                  };
+                });
+
+                # Zone makers
+                #   Each one gets a parent and edges which should activate it
+                #   The parent defines the area
+                #   The parents hotEdges defines a little space to be left, allowing dragging into the parent zone
+                mkNoSplit = parentZone: hotEdges: (
+                  let
+                    area = (mkArea parentZone.area parentZone.hotEdges);
+                  in { # 1 Zone
+                    inherit area hotEdges;
+                    full = (mkZone area "center");
+                  }
+                );
+                mkVerticalSplit = parentZone: hotEdges: (
+                  let
+                    area = (mkArea parentZone.area parentZone.hotEdges);
+                  in { # 2 vertical zones
+                    inherit area hotEdges;
+                    top = (mkZone {
+                      "x" = area."x";
+                      "y" = area."y";
+                      "width" = area."width";
+                      "height" = area."height" / 2;
+                    } "top-center");
+                    bottom = (mkZone {
+                      "x" = area."x";
+                      "y" = area."y" + (area."height" / 2);
+                      "width" = area."width";
+                      "height" = area."height" / 2;
+                    } "bottom-center");
+                  }
+                );
+                mkHorizontalSplit = parentZone: hotEdges: (
+                  let
+                    area = (mkArea parentZone.area parentZone.hotEdges);
+                  in { # 2 horizontal zones
+                    inherit area hotEdges;
+                    left = (mkZone {
+                      "x" = area."x";
+                      "y" = area."y";
+                      "width" = area."width" / 2;
+                      "height" = area."height";
+                    } "left-center");
+                    right = (mkZone {
+                      "x" = area."x" + (area."width" / 2);
+                      "y" = area."y";
+                      "width" = area."width" / 2;
+                      "height" = area."height";
+                    } "right-center");
+                  }
+                );
+                mkQuadriSplit = parentZone: hotEdges: (
+                  let
+                    area = (mkArea parentZone.area parentZone.hotEdges);
+                  in { # 4 zones in a grid
+                    inherit area hotEdges;
+                    topLeft = (mkZone {
+                      "x" = area."x";
+                      "y" = area."y";
+                      "width" = area."width" / 2;
+                      "height" = area."height" / 2;
+                    } "top-left");
+                    topRight = (mkZone {
+                      "x" = area."x" + (area."width" / 2);
+                      "y" = area."y";
+                      "width" = area."width" / 2;
+                      "height" = area."height" / 2;
+                    } "top-right");
+                    bottomLeft = (mkZone {
+                      "x" = area."x";
+                      "y" = area."y" + (area."height" / 2);
+                      "width" = area."width" / 2;
+                      "height" = area."height" / 2;
+                    } "bottom-left");
+                    bottomRight = (mkZone {
+                      "x" = area."x" + (area."width" / 2);
+                      "y" = area."y" + (area."height" / 2);
+                      "width" = area."width" / 2;
+                      "height" = area."height" / 2;
+                    } "bottom-right");
+                  }
+                );
+
+                # Zones
+                fullscreen = (mkNoSplit screen [ "top" ]);
+                verticals = (mkVerticalSplit fullscreen []); # Not used enough to justify hotEdges
+                quadriGrid = (mkQuadriSplit verticals []); # Not used enough to justify hotEdges
+                horizontals = (mkHorizontalSplit quadriGrid [ "left" "right" ]);
+
+              in [
+                # Fullscreen
+                fullscreen.full
+                # Verticals
+                verticals.top
+                verticals.bottom
+                # QuadriGrid
+                quadriGrid.topLeft
+                quadriGrid.topRight
+                quadriGrid.bottomLeft
+                quadriGrid.bottomRight
+                # Horizontals
+                horizontals.left
+                horizontals.right
+              ] # The ones at the bottom have more priority
+            );
           }
         }]";
       };
