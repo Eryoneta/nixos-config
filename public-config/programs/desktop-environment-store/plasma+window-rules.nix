0524d495ae0c39470d74d2d6334c671c70d4d5c0
Eryoneta <eryoneta@gmail.com>
2024-12-11 22:20:26 -0300

    [UPD] Programs/Plasma/KWin/Rules: Regras para janelas PiP de Firefox
    - (UPD) Adicionado regras para s janelas PiP de Firefox
    . Elas devem ficar sempre no topo de todas as outras

--- public-config/programs/desktop-environment-store/plasma+window-rules.nix
+++ public-config/programs/desktop-environment-store/plasma+window-rules.nix
@@ -14,8 +14,48 @@
       };
     };
     apply = { # What changes
-      desktopfile = { # Set the .desktop launcher
+      "desktopfile" = { # Set the .desktop launcher
         value = "codium";
+        apply = "initially"; # On start
+      };
+    };
+  }
+  {
+    description = "Stick Firefox PiP windows on top";
+    match = { # What target
+      window-class = {
+        value = "firefox firefox"; # Firefox
+        type = "exact";
+      };
+      window-types = [ "normal" ]; # Normal window
+      title = {
+        value = "Picture-in-Picture";
+        type = "exact";
+      };
+    };
+    apply = { # What changes
+      "above" = { # Set the window.to be above
+        value = true;
+        apply = "initially"; # On start
+      };
+    };
+  }
+  {
+    description = "Stick Firefox-Dev PiP windows on top";
+    match = { # What target
+      window-class = {
+        value = "firefox firefox-devedition"; # Firefox-Dev
+        type = "exact";
+      };
+      window-types = [ "normal" ]; # Normal window
+      title = {
+        value = "Picture-in-Picture";
+        type = "exact";
+      };
+    };
+    apply = { # What changes
+      "above" = { # Set the window.to be above
+        value = true;
         apply = "initially"; # On start
       };
     };
