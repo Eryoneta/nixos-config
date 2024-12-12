9f4f3f7d2e2921834fa51cce2b197a21bada65c1
Eryoneta <eryoneta@gmail.com>
2024-12-11 21:04:24 -0300

    [UPD] Programs/VSCodium: Configuração automática
    - (UPD) O programa requer 'vsintellicode.modify.editor.suggestSelection' configurado

--- public-config/programs/app-store/vscodium.home.default.nix
+++ public-config/programs/app-store/vscodium.home.default.nix
@@ -30,6 +30,8 @@
           "workbench.editor.pinnedTabsOnSeparateRow" = true; # Pinned tabs on a separated row!
           "editor.renderWhitespace" = "all"; # Show spaces as dots
           "window.titleBarStyle" = "custom"; # Do not use the OS decorations (Smaller context menu)
+          # Automatic settings
+          "vsintellicode.modify.editor.suggestSelection" = "automaticallyOverrodeDefaultValue";
         }
       );
 
