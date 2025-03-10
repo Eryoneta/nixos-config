7cfc4d528a1d0ba5fad59367b3b3f4b17414a079
Eryoneta <eryoneta@gmail.com>
2025-03-10 13:45:13 -0300

    [UPD] Programs/ZSH: Alias de upgrade
    - (UPD) Adicionado alias para 'nix flake update' e 'nixos-rebuild switch'

--- public-config/programs/shell-store/zsh.home.yo.nix
+++ public-config/programs/shell-store/zsh.home.yo.nix
@@ -1,4 +1,4 @@
-{ lib, config, userDev, ... }@args: with args.config-utils; {
+{ lib, config, userDev, auto-upgrade-pkgs, ... }@args: with args.config-utils; {
   config = with config.profile.programs.zsh; (lib.mkIf (options.enabled) {
 
     # ZSH: Shell
@@ -7,13 +7,13 @@
       # Aliases
       shellAliases = (
         let
-          systemProfile = rec {
+          systemProfile = {
             name = "system";
-            path = "/nix/var/nix/profiles/${name}";
+            path = "/nix/var/nix/profiles/${systemProfile.name}";
           };
-          upgradeProfile = rec {
+          upgradeProfile = {
             name = "System_Upgrades";
-            path = "/nix/var/nix/profiles/system-profiles/${name}";
+            path = "/nix/var/nix/profiles/system-profiles/${upgradeProfile.name}";
           };
 
           rebuildSystemCommand = rebuildMode: (
@@ -21,9 +21,9 @@
               promptSudo = "sudo ls /dev/null > /dev/null 2>&1"; # Makes the sudo prompt appear before (If later, nom hides the prompt)
               nixosRebuild = "sudo nixos-rebuild";
               flakePath = "path:${userDev.configDevFolder}#${userDev.name}@${userDev.host.name}"; # "path:" = Ignores Git repository
-              args = "--use-remote-sudo --show-trace --print-build-logs --verbose";
+              args = "--flake \"${flakePath}\" --use-remote-sudo --show-trace --print-build-logs --verbose";
               nomOutput = "|& nom"; # nix-output-monitor
-            in "${promptSudo} && ${nixosRebuild} ${rebuildMode} --flake ${flakePath} ${args} ${nomOutput}"
+            in "${promptSudo} && ${nixosRebuild} ${rebuildMode} ${args} ${nomOutput}"
           );
 
           rebuildHomeCommand = (
@@ -31,8 +31,46 @@
               homeManagerRebuild = "home-manager";
               rebuildMode = "switch";
               flakePath = "path:${userDev.configDevFolder}#${userDev.name}@${userDev.host.name}"; # "path:" = Ignores Git repository
-              args = "--print-build-logs --verbose";
-            in "${homeManagerRebuild} ${rebuildMode} --flake ${flakePath} ${args}"
+              args = "--flake \"${flakePath}\" --print-build-logs --verbose";
+            in "${homeManagerRebuild} ${rebuildMode} ${args}"
+          );
+
+          upgradeSystemCommand = (
+            let
+              updateFlakeLockCommand = (
+                let
+                  inputs = (builtins.toString auto-upgrade-pkgs);
+                  configFolderPath = userDev.configFolder;
+                  # Important note: This whole command should be equivalent as set by "modules/nixos-modules/auto-upgrade-update-flake-lock.nix"!
+                in (utils.replaceStr "\n" "" ''
+                  ufl() {
+                    echo "Updating flake.lock...";
+                    nix flake update ${inputs} --flake "${configFolderPath}" --commit-lock-file;
+                    cd "${configFolderPath}";
+                    if [[ $(git diff --name-only HEAD HEAD~1) == "flake.lock" ]]; then
+                      git commit --amend --no-edit --author="NixOS AutoUpgrade <nixos@${userDev.host.name}>";
+                    fi;
+                    cd -;
+                  };
+                  ufl
+                '')
+                
+              );
+              rebuildSystemCommand = (
+                # Important note: This command should be equivalent as set by "hosts/default/features/auto-upgrade.nix"!
+                let
+                  promptSudo = "sudo ls /dev/null > /dev/null 2>&1"; # Makes the sudo prompt appear before (If later, nom hides the prompt)
+                  nixosRebuild = "sudo nixos-rebuild";
+                  rebuildMode = "switch";
+                  flakePath = "git+file://${userDev.configFolder}?submodules=1#${userDev.name}@${userDev.host.name}";
+                  # Notice: The flake ignores submodules! The flag "submodules=1" is necessary
+                  # TODO: (Config/AutoUpgrade) Remove once submodules are supported by default
+                  args = "--flake \"${flakePath}\" --profile-name ${upgradeProfile.name} --use-remote-sudo --show-trace --print-build-logs --verbose";
+                  nomOutput = "|& nom"; # nix-output-monitor
+                in "${promptSudo} && ${nixosRebuild} ${rebuildMode} ${args} ${nomOutput}"
+              );
+            in "${updateFlakeLockCommand} && ${rebuildSystemCommand}"
+            # TODO: (ZSH) Isn't easier to just call nixos-upgrade? Yeah. but it doesn't give outputs... eh
           );
 
           list = profileName: (
@@ -65,6 +103,7 @@
           collectGarbageCommand = "nix-collect-garbage";
 
           nxCommand = (
+            # Note: Everything is collapsed into a single line
             utils.replaceStr "\n" "" ''
               nx() {
                 case $1 in
@@ -82,6 +121,9 @@
                   ;;
                   "home")
                     ${rebuildHomeCommand};
+                  ;;
+                  "upgrade")
+                    ${upgradeSystemCommand};
                   ;;
                   "list")
                     ${list "${systemProfile.name}"};
