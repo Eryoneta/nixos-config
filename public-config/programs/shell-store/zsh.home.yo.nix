7551541597c9e1a01286b6513ed8b8579f51d6b4
Eryoneta <eryoneta@gmail.com>
2025-03-10 15:50:33 -0300

    [UPD] Programs/ZSH: Aliases com subcomando sys
    - (UPD) Adicionado subcomando 'sys', que afeta as builds de 'AutoUpgrade'
    . Permite criar, deletar, e listar gerações de 'System_Upgrades'

--- public-config/programs/shell-store/zsh.home.yo.nix
+++ public-config/programs/shell-store/zsh.home.yo.nix
@@ -10,79 +10,89 @@
           systemProfile = {
             name = "system";
             path = "/nix/var/nix/profiles/${systemProfile.name}";
+            flakePath = "path:${userDev.configDevFolder}#${userDev.name}@${userDev.host.name}"; # "path:" = Ignores Git repository
           };
           upgradeProfile = {
             name = "System_Upgrades";
             path = "/nix/var/nix/profiles/system-profiles/${upgradeProfile.name}";
+            flakePath = "git+file://${userDev.configFolder}?submodules=1#${userDev.name}@${userDev.host.name}";
+            # Notice: The flake ignores submodules! The flag "submodules=1" is necessary
+            # TODO: (Config/AutoUpgrade) Remove once submodules are supported by default
           };
 
           rebuildSystemCommand = rebuildMode: (
             let
               promptSudo = "sudo ls /dev/null > /dev/null 2>&1"; # Makes the sudo prompt appear before (If later, nom hides the prompt)
-              nixosRebuild = "sudo nixos-rebuild";
-              flakePath = "path:${userDev.configDevFolder}#${userDev.name}@${userDev.host.name}"; # "path:" = Ignores Git repository
-              args = "--flake \"${flakePath}\" --use-remote-sudo --show-trace --print-build-logs --verbose";
+              args = "--use-remote-sudo --show-trace --print-build-logs --verbose";
               nomOutput = "|& nom"; # nix-output-monitor
-            in "${promptSudo} && ${nixosRebuild} ${rebuildMode} ${args} ${nomOutput}"
+              rebuildSystem = (utils.replaceStr "\n" "" ''
+                rs() {
+                  local profileNameArg;
+                  local flakePathArg;
+                  if [[ $2 =~ ^sys$ ]]; then
+                    profileNameArg="--profile-name ${upgradeProfile.name}";
+                    flakePathArg="--flake \"${upgradeProfile.flakePath}\"";
+                  else
+                    profileNameArg="--profile-name ${systemProfile.name}";
+                    flakePathArg="--flake \"${systemProfile.flakePath}\"";
+                  fi;
+                  eval "sudo nixos-rebuild ${rebuildMode} $flakePathArg $profileNameArg ${args} ${nomOutput}";
+                };
+                rs
+              '');
+            in "${promptSudo} && ${rebuildSystem}"
+            
           );
 
           rebuildHomeCommand = (
             let
-              homeManagerRebuild = "home-manager";
-              rebuildMode = "switch";
-              flakePath = "path:${userDev.configDevFolder}#${userDev.name}@${userDev.host.name}"; # "path:" = Ignores Git repository
-              args = "--flake \"${flakePath}\" --print-build-logs --verbose";
-            in "${homeManagerRebuild} ${rebuildMode} ${args}"
+              args = "--flake \"${systemProfile.flakePath}\" --print-build-logs --verbose";
+            in "home-manager switch ${args}"
           );
 
           upgradeSystemCommand = (
             let
-              updateFlakeLockCommand = (
-                let
-                  inputs = (builtins.toString auto-upgrade-pkgs);
-                  configFolderPath = userDev.configFolder;
-                  # Important note: This whole command should be equivalent as set by "modules/nixos-modules/auto-upgrade-update-flake-lock.nix"!
-                in (utils.replaceStr "\n" "" ''
-                  ufl() {
-                    echo "Updating flake.lock...";
-                    nix flake update ${inputs} --flake "${configFolderPath}" --commit-lock-file;
-                    cd "${configFolderPath}";
-                    if [[ $(git diff --name-only HEAD HEAD~1) == "flake.lock" ]]; then
-                      git commit --amend --no-edit --author="NixOS AutoUpgrade <nixos@${userDev.host.name}>";
-                    fi;
-                    cd -;
-                  };
-                  ufl
-                '')
-                
-              );
-              rebuildSystemCommand = (
-                # Important note: This command should be equivalent as set by "hosts/default/features/auto-upgrade.nix"!
-                let
-                  promptSudo = "sudo ls /dev/null > /dev/null 2>&1"; # Makes the sudo prompt appear before (If later, nom hides the prompt)
-                  nixosRebuild = "sudo nixos-rebuild";
-                  rebuildMode = "switch";
-                  flakePath = "git+file://${userDev.configFolder}?submodules=1#${userDev.name}@${userDev.host.name}";
-                  # Notice: The flake ignores submodules! The flag "submodules=1" is necessary
-                  # TODO: (Config/AutoUpgrade) Remove once submodules are supported by default
-                  args = "--flake \"${flakePath}\" --profile-name ${upgradeProfile.name} --use-remote-sudo --show-trace --print-build-logs --verbose";
-                  nomOutput = "|& nom"; # nix-output-monitor
-                in "${promptSudo} && ${nixosRebuild} ${rebuildMode} ${args} ${nomOutput}"
-              );
-            in "${updateFlakeLockCommand} && ${rebuildSystemCommand}"
-            # TODO: (ZSH) Isn't easier to just call nixos-upgrade? Yeah. but it doesn't give outputs... eh
+              inputs = (builtins.toString auto-upgrade-pkgs);
+              configFolderPath = userDev.configFolder;
+              # Important note: This whole command should be equivalent as set by "modules/nixos-modules/auto-upgrade-update-flake-lock.nix"!
+            in (utils.replaceStr "\n" "" ''
+              ufl() {
+                echo "Updating flake.lock...";
+                nix flake update ${inputs} --flake "${configFolderPath}" --commit-lock-file;
+                cd "${configFolderPath}";
+                if [[ $(git diff --name-only HEAD HEAD~1) == "flake.lock" ]]; then
+                  git commit --amend --no-edit --author="NixOS AutoUpgrade <nixos@${userDev.host.name}>";
+                fi;
+                cd -;
+              };
+              ufl
+            '')
           );
 
-          list = profileName: (
-            let
-              nixosRebuild = "nixos-rebuild";
-              profile = if(profileName != "") then "--profile-name ${profileName}" else "";
-            in "${nixosRebuild} list-generations ${profile}"
+          listGenerationsCommand = (
+            utils.replaceStr "\n" "" ''
+              lg() {
+                local profileNameArg;
+                if [[ $2 =~ ^sys$ ]]; then
+                  profileNameArg="--profile-name ${upgradeProfile.name}";
+                else
+                  profileNameArg="";
+                fi;
+                eval "nixos-rebuild list-generations $profileNameArg";
+              };
+              lg
+            ''
           );
 
-          deleteGenerationsCommand = profilePath: (
+          deleteGenerationsCommand = (
             utils.replaceStr "\n" "" ''
               dg() {
+                local profilePath;
+                if [[ $2 =~ ^sys$ ]]; then
+                  profilePath=${upgradeProfile.path};
+                else
+                  profilePath=${systemProfile.path};
+                fi;
                 local generations;
                 for command in "$@"; do
                   if [[ $command =~ ^[0-9]+$ ]]; then
@@ -91,7 +101,7 @@
                 done;
                 echo "";
                 echo "Deleting system generations...";
-                eval "sudo nix-env --delete-generations $generations --profile ${profilePath}";
+                eval "sudo nix-env --delete-generations $generations --profile $profilePath";
                 echo "";
                 echo "Deleting ALL home-manager generations...";
                 home-manager expire-generations "-0 day";
@@ -108,16 +118,16 @@
               nx() {
                 case $1 in
                   "boot")
-                    ${rebuildSystemCommand "boot"};
+                    ${rebuildSystemCommand "boot"} $@;
                   ;;
                   "test")
-                    ${rebuildSystemCommand "test"};
+                    ${rebuildSystemCommand "test"} $@;
                   ;;
                   "switch")
-                    ${rebuildSystemCommand "switch"};
+                    ${rebuildSystemCommand "switch"} $@;
                   ;;
                   "build-vm")
-                    ${rebuildSystemCommand "build-vm"};
+                    ${rebuildSystemCommand "build-vm"} $@;
                   ;;
                   "home")
                     ${rebuildHomeCommand};
@@ -126,18 +136,12 @@
                     ${upgradeSystemCommand};
                   ;;
                   "list")
-                    ${list "${systemProfile.name}"};
+                    ${listGenerationsCommand} $@;
                   ;;
-                  "listsys")
-                    ${list "${upgradeProfile.name}"};
+                  "degen")
+                    ${deleteGenerationsCommand} $@;
                   ;;
-                  "delgen")
-                    ${deleteGenerationsCommand "${systemProfile.path}"} $@;
-                  ;;
-                  "delgensys")
-                    ${deleteGenerationsCommand "${upgradeProfile.path}"} $@;
-                  ;;
-                  "cg")
+                  "trim")
                     ${collectGarbageCommand};
                   ;;
                 esac;
