00c69bb33ab1119a1ed5a954e0fd142374704f1b
Eryoneta <eryoneta@gmail.com>
2025-03-17 22:50:42 -0300

    [ADD] Programs/Python: pandas, openpyxl
    - (ADD) Bibliotecas para lidar com dados e arquivos 'Excel'

--- public-config/programs/dev-programs.home.default.nix
+++ public-config/programs/dev-programs.home.default.nix
@@ -10,11 +10,17 @@
         ++
         (with stable; [
           (lib.hiPrio zulu17) # Zulu: Java Development Kit v17
-          python3Full # Python: Full Python environment v3
+          # Note: Has a collision with "eclipses.eclipse-java"
+
+          (python3.withPackages (py-pkgs: [ # Python: Full Python environment v3
+            py-pkgs.pandas # Pandas: Library for data structures
+            py-pkgs.openpyxl # OpenPyXL: Library to read Microsoft Excel Spreadsheet files
+          ]))
 
           eclipses.eclipse-java # Eclipse(For Java): IDE for Java development
           nodePackages."@angular/cli" # Angular-Cli: Helper for Angular development
           hoppscotch # Hoppscotch: API tool
+
           mysql-workbench # MySQL Workbench: Tool for MySQL management
         ])
       );
