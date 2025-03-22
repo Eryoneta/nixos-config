19dac4a62f2acbafdb5001a7aae9f97ea61c01b8
Eryoneta <eryoneta@gmail.com>
2025-03-22 12:16:32 -0300

    [ADD] Programs: DJango, MySQLClient
    - (ADD) Framework para desenvolvimento web com 'Python'
    - (ADD) Conector com de 'Python' com 'MySQL'

--- public-config/programs/dev-programs.home.default.nix
+++ public-config/programs/dev-programs.home.default.nix
@@ -12,10 +12,13 @@
           (lib.hiPrio zulu17) # Zulu: Java Development Kit v17
           # Note: Has a collision with "eclipses.eclipse-java"
 
-          (python3.withPackages (py-pkgs: [ # Python: Full Python environment v3
-            py-pkgs.pandas # Pandas: Library for data structures
-            py-pkgs.openpyxl # OpenPyXL: Library to read Microsoft Excel Spreadsheet files
-          ]))
+          (python3.withPackages (py-pkgs: (with py-pkgs; [ # Python: Full Python environment v3
+            pandas # Pandas: Library for data structures
+            openpyxl # OpenPyXL: Library to read Microsoft Excel Spreadsheet files
+
+            django # DJango: Framework for web-development with Python
+            mysqlclient # MySQL Client: Allows for communication with a MySQL server
+          ])))
 
           eclipses.eclipse-java # Eclipse(For Java): IDE for Java development
           nodePackages."@angular/cli" # Angular-Cli: Helper for Angular development
