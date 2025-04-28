87b3940ee02c8a010d0c92cc5b828288267a7e5a
Eryoneta <eryoneta@gmail.com>
2025-04-28 00:37:24 -0300

    [UPD] NixOS-Modules/AutoUpgrade-Notifier: Mais opções
    - (UPD) Adicionado mais opções no módulo
    . É parecido com GarbageCollector-Notifier

--- hosts/default/features/auto-upgrade.nix
+++ hosts/default/features/auto-upgrade.nix
@@ -19,6 +19,7 @@
       # Notice: The flake ignores submodules! The flag "submodules=1" is necessary
       # TODO: (Config/AutoUpgrade) Remove once submodules are supported by default
 
+      # Git support ("auto-upgrade-git-support.nix")
       gitSupport = {
         enable = (utils.mkDefault) true;
         systemUser = host.userDev.username;
@@ -27,23 +28,32 @@
         push = (utils.mkDefault) true;
       };
 
+      # Update flake lock ("auto-upgrade-update-flake-lock.nix")
       updateFlakeLock = {
         enable = (utils.mkDefault) true;
         inputs = auto-upgrade-pkgs;
         commitLockFile = (utils.mkDefault) true; # Commits "flake.lock"
       };
 
+      # Alter profile ("auto-upgrade-alter-profile.nix")
       alterProfile = {
         enable = (utils.mkDefault) true;
         name = (utils.mkDefault) "System_Upgrades";
         configurationLimit = (utils.mkDefault) 12; # 12 seems like a good mumber, 3 months of weekly upgrades
       };
 
+      # Notifier ("auto-upgrade-notifier.nix")
       notifier = {
         enable = (utils.mkDefault) true;
         systemUser = host.userDev.username;
-        informStart = (utils.mkDefault) true;
-        informConclusion = (utils.mkDefault) true;
+          informStart = {
+            show = (utils.mkDefault) true;
+            time = (utils.mkDefault) 15;
+          };
+          informConclusion = {
+            show = (utils.mkDefault) true;
+            time = (utils.mkDefault) "infinite";
+          };
       };
 
     };
--- modules/nixos-modules/auto-upgrade-notifier.nix
+++ modules/nixos-modules/auto-upgrade-notifier.nix
@@ -3,7 +3,7 @@
   - Creates a notification for every upgrade made
     - Great for knowing if the upgrade was a success or not
   - Uses "libnotify"
-  - Depends on "nixpkgs/nixos/modules/tasks/auto-upgrade.nix"
+  - Depends on the option set by "nixpkgs/nixos/modules/tasks/auto-upgrade.nix"
 */
 { config, pkgs, lib, ... }:
   let
@@ -24,16 +24,38 @@
           default = "root";
         };
 
-        informStart = lib.mkOption {
-          type = lib.types.bool;
-          description = "Shows a notification when a upgrade starts.";
-          default = true;
+        informStart = {
+          show = lib.mkOption {
+            type = lib.types.bool;
+            description = "Show notification when a upgrade starts.";
+            default = true;
+          };
+          time = lib.mkOption {
+            type = lib.types.either (lib.types.ints.between 1 86400) (lib.types.enum [ "infinite" ]);
+            description = ''
+              Time in seconds to show the notification.
+
+              Can be either a number between 1 and 86400, or "infinite"
+            '';
+            default = 15;
+          };
         };
 
-        informConclusion = lib.mkOption {
-          type = lib.types.bool;
-          description = "Shows a notification when a upgrade ends, informing success or failure.";
-          default = true;
+        informConclusion = {
+          show = lib.mkOption {
+            type = lib.types.bool;
+            description = "Shows a notification when a upgrade ends, informing success or failure.";
+            default = true;
+          };
+          time = lib.mkOption {
+            type = lib.types.either (lib.types.ints.between 1 86400) (lib.types.enum [ "infinite" ]);
+            description = ''
+              Time in seconds to show the notification.
+
+              Can be either a number between 1 and 86400, or "infinite"
+            '';
+            default = "infinite";
+          };
         };
 
       };
@@ -42,88 +64,95 @@
     config = lib.mkIf (cfg.enable && cfg_n.enable) {
 
       assertions = [{
-        assertion = !(!cfg_n.informStart && !cfg_n.informConclusion);
+        assertion = !(!cfg_n.informStart.show && !cfg_n.informConclusion.show);
         message = ''
-          The option 'system.autoUpgrade.notifier.enable' requires at least one of 'informStart' or 'informConclusion' to be set.
+          The option 'system.autoUpgrade.notifier.enable' requires at least one of 'informStart.show' or 'informConclusion.show' to be set.
         '';
       }];
 
-      # Notify upgrade start
-      systemd.services."nixos-upgrade-notify-start" = lib.mkIf (cfg_n.informStart) {
-        serviceConfig = {
-          "Type" = "oneshot";
-          "User" = cfg_n.systemUser;
-        };
-        path = with pkgs; [
-          coreutils
-          libnotify
-        ];
-        script = ''
-          # Interrupts if there is an error or undefined variable
-          set -eu
-          # Loads access
-          export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/''${UID}/bus"
-          # Send notification
-          notify-send "NixOS Upgrade" \
-            "Performing system upgrade...\nIt should be available in the next boot" \
-            --urgency "normal" --expire-time 15000 --icon "system-upgrade"
-        '';
-        # Note: The notification is shown for 15 seconds
-      };
+      systemd = (
+        let
+          mkScript = {text, icon, timing}: ''
+            # Interrupts if there is an error or undefined variable
+            set -eu
+            # Loads access
+            export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/''${UID}/bus"
+            # Send notification
+            notify-send "NixOS Upgrade" \
+              "${text}" \
+              --icon "${icon}" \
+              --urgency "${timing.urgency}" \
+              --expire-time ${timing.expireTime}
+          '';
+          mkTiming = time: {
+            urgency = if ((builtins.typeOf time) == "string" && time == "infinite") then "critical" else "normal";
+            expireTime = (builtins.toString (
+              (if ((builtins.typeOf time) == "int" && time >= 1) then time else 1) * 1000) # Seconds -> Miliseconds
+            );
+          };
+        in {
 
-      # Notify upgrade success
-      systemd.services."nixos-upgrade-notify-success" = lib.mkIf (cfg_n.informConclusion) {
-        serviceConfig = {
-          "Type" = "oneshot";
-          "User" = cfg_n.systemUser;
-        };
-        path = with pkgs; [
-          coreutils
-          libnotify
-        ];
-        script = ''
-          # Interrupts if there is an error or undefined variable
-          set -eu
-          # Loads access
-          export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/''${UID}/bus"
-          # Send notification
-          notify-send "NixOS Upgrade" \
-            "The system upgrade was successful" \
-            --urgency "critical" --icon "system-upgrade"
-        '';
-        # Note: The notification is shown until dismissed
-      };
+          # Notify upgrade start
+          services."nixos-upgrade-notify-start" = lib.mkIf (cfg_n.informStart.show) {
+            serviceConfig = {
+              "Type" = "oneshot";
+              "User" = cfg_n.systemUser;
+            };
+            path = with pkgs; [
+              coreutils
+              libnotify
+            ];
+            script = (mkScript {
+              text = "Performing system upgrade...\nIt should be available in the next boot";
+              icon = "system-upgrade";
+              timing = (mkTiming cfg_n.informStart.time);
+            });
+          };
 
-      # Notify upgrade success
-      systemd.services."nixos-upgrade-notify-failure" = lib.mkIf (cfg_n.informConclusion) {
-        serviceConfig = {
-          "Type" = "oneshot";
-          "User" = cfg_n.systemUser;
-        };
-        path = with pkgs; [
-          coreutils
-          libnotify
-        ];
-        script = ''
-          # Interrupts if there is an error or undefined variable
-          set -eu
-          # Loads access
-          export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/''${UID}/bus"
-          # Send notification
-          notify-send "NixOS Upgrade" \
-            "The system upgrade failed" \
-            --urgency "critical" --icon "script-error"
-        '';
-        # Note: The notification is shown until dismissed
-      };
+          # Notify upgrade success
+          services."nixos-upgrade-notify-success" = lib.mkIf (cfg_n.informConclusion.show) {
+            serviceConfig = {
+              "Type" = "oneshot";
+              "User" = cfg_n.systemUser;
+            };
+            path = with pkgs; [
+              coreutils
+              libnotify
+            ];
+            script = (mkScript {
+              text = "The system upgrade was successful";
+              icon = "system-upgrade";
+              timing = (mkTiming cfg_n.informConclusion.time);
+            });
+          };
 
-      # NixOS-Upgrade calls the services as needed
-      systemd.services."nixos-upgrade" = {
-        wants = [ "nixos-upgrade-notify-start.service" ];
-        after = [ "nixos-upgrade-notify-start.service" ];
-        onSuccess = [ "nixos-upgrade-notify-success.service" ];
-        onFailure = [ "nixos-upgrade-notify-failure.service" ];
-      };
+          # Notify upgrade failure
+          services."nixos-upgrade-notify-failure" = lib.mkIf (cfg_n.informConclusion.show) {
+            serviceConfig = {
+              "Type" = "oneshot";
+              "User" = cfg_n.systemUser;
+            };
+            path = with pkgs; [
+              coreutils
+              libnotify
+            ];
+            script = (mkScript {
+              text = "The system upgrade failed";
+              icon = "script-error";
+              timing = (mkTiming cfg_n.informConclusion.time);
+            });
+          };
+
+          # NixOS-Upgrade calls the services as needed
+          services."nixos-upgrade" = {
+            wants = (lib.optional (cfg_n.informStart.show) "nixos-upgrade-notify-start.service");
+            after = (lib.optional (cfg_n.informStart.show) "nixos-upgrade-notify-start.service");
+            onSuccess = (lib.optional (cfg_n.informConclusion.show) "nixos-upgrade-notify-success.service");
+            onFailure = (lib.optional (cfg_n.informConclusion.show) "nixos-upgrade-notify-failure.service");
+          };
+
+        }
+      );
 
     };
   }
