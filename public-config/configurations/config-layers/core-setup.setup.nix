081f39bbb53b6b2dd15d650d701aa3c32c5c6375
Eryoneta <eryoneta@gmail.com>
2025-05-29 16:42:05 -0300

    (WIP)[ALT] Configurations: Mais opções em Core-Setup
    - (ALT) Movido configurações de Networking para Core-Setup
    . Essas configurações são importantes

--- public-config/configurations/config-layers/core-setup.setup.nix
+++ public-config/configurations/config-layers/core-setup.setup.nix
@@ -8,6 +8,35 @@
       nixos = { host, ... }: { # (NixOS Module)
         config = {
 
+          # Hostname
+          networking.hostName = host.name;
+
+          # Network access
+          networking.networkmanager.enable = (utils.mkDefault) true;
+
+          # Time zone
+          time.timeZone = (utils.mkDefault) "America/Sao_Paulo";
+
+          # Locale
+          i18n = (
+            let
+              locale = "pt_BR.UTF-8";
+            in {
+              defaultLocale = (utils.mkDefault) locale;
+              extraLocaleSettings = (utils.mkDefault) {
+                "LC_ADDRESS" = locale;
+                "LC_IDENTIFICATION" = locale;
+                "LC_MEASUREMENT" = locale;
+                "LC_MONETARY" = locale;
+                "LC_NAME" = locale;
+                "LC_NUMERIC" = locale;
+                "LC_PAPER" = locale;
+                "LC_TELEPHONE" = locale;
+                "LC_TIME" = locale;
+              };
+            }
+          );
+
           # Start version
           system.stateVersion = "${host.system.stateVersion}"; # NixOS start version. (Default options)
 
