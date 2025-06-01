d2b5351286ed50a303bd89184a5990c458b9a5ff
Eryoneta <eryoneta@gmail.com>
2025-06-01 13:31:41 -0300

    (WIP)[ADD] Programs: Tag 'interest'
    - (ADD) Adicionado nova tag 'interest'
    . Para coisas de entretenimento

---
+++ public-config/programs/apps/calibre.nix
@@ -0,0 +1,26 @@
+{ pkgs-bundle, ... }@args: with args.config-utils; { # Setup Module
+
+  # Calibre: E-Book manager
+  config.modules."calibre" = {
+    attr.packageChannel = pkgs-bundle.stable;
+    tags = [ "yo" "interest" ];
+    setup = { attr }: {
+      home = { config-domain, ... }: { # Home-Manager Module
+
+        # Install
+        config.home.packages = with attr.packageChannel; [ calibre ];
+
+        # Dotfiles
+        config.xdg.configFile."calibre" = with config-domain; {
+          # Check for "./private-config/dotfiles"
+          enable = (utils.pathExists private.dotfiles);
+          source = with outOfStore.private; (
+            utils.mkOutOfStoreSymlink "${dotfiles}/calibre/.config/calibre"
+          );
+        };
+
+      };
+    };
+  };
+
+}
