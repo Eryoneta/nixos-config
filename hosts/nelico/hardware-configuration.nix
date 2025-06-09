893ebf137a7c67bccd60bfc7d737957b2e6133a1
Eryoneta <eryoneta@gmail.com>
2025-06-08 23:25:29 -0300

    [ALT] Hosts/NeLiCo: Presença de drives opcional
    - (ALT) Os drives agora não precisam estar presentes para o sistema ser ativado
    . Apenas principal, Main, deve estar conectado
    . Storage e Mirror são opcionais

--- hosts/nelico/hardware-configuration.nix
+++ hosts/nelico/hardware-configuration.nix
@@ -4,7 +4,7 @@
   config.modules."nelico-hardware" = {
     tags = config.modules."nelico".tags;
     setup = {
-      nixos = { config, modulesPath, ... }: { # (NixOS Module)
+      nixos = { config, modulesPath, host, ... }: { # (NixOS Module)
 
         # For the not-detected hardware
         imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
@@ -18,38 +18,41 @@
         config.boot.extraModulePackages = [ config.boot.kernelPackages.rtl8192eu ];
 
         # Personal partition
-        config.fileSystems."/home/yo/Personal" = {
+        config.fileSystems."/home/${host.userDev.username}/Personal" = {
           device = "/dev/disk/by-label/Personal";
           fsType = "ext4";
         };
 
         # Basement partition
-        config.fileSystems."/home/yo/Personal/Mind Mansion/Basement" = {
+        config.fileSystems."/home/${host.userDev.username}/Personal/Mind Mansion/Basement" = {
           device = "/dev/disk/by-label/Basement";
           fsType = "ext4";
-          depends = [ "/home/yo/Personal" ];
+          depends = [ "/home/${host.userDev.username}/Personal" ];
+          options = [ "nofail" ]; # Can be absent
         };
 
         # Storage partition
-        config.fileSystems."/home/yo/Personal/System_Utilities/Storage" = {
+        config.fileSystems."/home/${host.userDev.username}/Personal/System_Utilities/Storage" = {
           device = "/dev/disk/by-label/Storage";
           fsType = "ext4";
-          depends = [ "/home/yo/Personal" ];
+          depends = [ "/home/${host.userDev.username}/Personal" ];
+          options = [ "nofail" ]; # Can be absent
         };
 
         # Mirror partition
-        config.fileSystems."/home/yo/Personal/System_Utilities/Backups/Mirror" = {
+        config.fileSystems."/home/${host.userDev.username}/Personal/System_Utilities/Backups/Mirror" = {
           device = "/dev/disk/by-label/Mirror";
           fsType = "ext4";
-          depends = [ "/home/yo/Personal" ];
+          depends = [ "/home/${host.userDev.username}/Personal" ];
+          options = [ "nofail" ]; # Can be absent
         };
 
         # Firmware
         config.hardware.cpu.intel.updateMicrocode = (utils.mkDefault) config.hardware.enableRedistributableFirmware;
 
-        # Hardware-Fix: Enable SYSRQ for REISUB
+        # Hardware-Fix: Enable SYSRQ for "R-E-I-S-U-B"
         config.boot.kernel.sysctl."kernel.sysrq" = 1;
-        # Note: KWin might freeze into a loop when dragging a window. Rare, but VERY annoying!
+        # Note: Sometimes, KWin might fatally freeze
 
       };
     };
