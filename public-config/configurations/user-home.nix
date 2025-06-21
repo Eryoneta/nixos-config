d4d5067f65bacaae61b937527c5aeeab9a1e0464
Eryoneta <eryoneta@gmail.com>
2025-06-21 17:28:34 -0300

    [UPD] Configuration: Agrupamento em 'user-home'
    - (UPD) Agrupado configurações de 'desktop entries', 'user directories', e 'file associations' em 'user-home'
    . Isso agrupa a configuração do home de um usuário

---
+++ public-config/configurations/user-home.nix
@@ -0,0 +1,47 @@
+{ ... }@args: with args.config-utils; { # (Setup Module)
+
+  # User Home
+  config.modules."user-home" = {
+    tags = [ "default-setup" ];
+    setup = {
+      home = { config, ... }: { # (Home-Manager Module)
+
+        # XDG Specification
+        config.xdg.enable = true;
+
+        # XDG Base Directory
+        config.xdg.userDirs = {
+          enable = true;
+          createDirectories = true; # Auto-create directories
+        } // (
+          let
+            homePath = config.home.homeDirectory;
+          in {
+            desktop = "${homePath}/Área de Trabalho";
+            documents = "${homePath}/Documentos";
+            download = "${homePath}/Downloads";
+            music = "${homePath}/Músicas";
+            pictures = "${homePath}/Imagens";
+            videos = "${homePath}/Vídeos";
+            publicShare = "${homePath}/Público";
+            templates = "${homePath}/Modelos";
+          }
+        );
+
+        # XDG Desktop Entries
+        config.xdg.desktopEntries = {
+
+          # Hide "NixOS Manual" entry
+          "nixos-manual" = {
+            name = "NixOS Manual";
+            exec = "nixos-help";
+            noDisplay = true;
+          };
+
+        };
+
+      };
+    };
+  };
+
+}
