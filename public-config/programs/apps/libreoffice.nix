7104e52d20a5067a051189ac10f3203ac01af118
Eryoneta <eryoneta@gmail.com>
2025-06-21 14:46:30 -0300

    [FIX] Programs/(LibreOffice, Hoppscotch): Desktop entries
    - (FIX) Hoppscotch: Adicionado na categoria 'Development'
    . Sem categorias, o app fica em 'Achados e Perdidos'
    - (ALT) LibreOffice: Removido da categoria de 'Education'
    . Era o único app na categoria

--- public-config/programs/apps/libreoffice.nix
+++ public-config/programs/apps/libreoffice.nix
@@ -9,11 +9,41 @@
 
         # Configuration
         config.home.packages = with attr.packageChannel; [
-          libreoffice # LibreOffice
+          (utils.lowerPriority libreoffice) # LibreOffice
+          # Note: Necessary to be able to change desktop entries
           hunspell # Spell checker
           hunspellDicts.pt_BR # Spell dictionary
           hunspellDicts.en_US # Spell dictionary
         ];
+
+        # Desktop Entry: Recreate without the "Education" category
+        config.xdg.desktopEntries."math" = {
+          name = "LibreOffice Math";
+          exec = "libreoffice --math %U";
+          icon = "libreoffice-math";
+          categories = [ "Office" ];
+          type = "Application";
+          terminal = false;
+          mimeType = [
+            "application/mathml+xml"
+            "application/vnd.oasis.opendocument.formula"
+            "application/vnd.oasis.opendocument.formula-template"
+            "application/vnd.stardivision.math"
+            "application/vnd.sun.xml.math"
+            "application/x-starmath"
+            "text/mathml"
+          ];
+          settings = {
+            "GenericName" = "Formula Editor";
+            "GenericName[pt_BR]" = "Editor de fórmulas";
+            "Comment" = "Create and edit scientific formulas and equations.";
+            "Comment[pt_BR]" = "Crie e edite fórmulas científicas e equações.";
+            "StartupNotify" = "true";
+          };
+          # Note: As defined by the package
+          #   (Can be seen at "~/.local/state/nix/profiles/home-manager-<GENERATION>-link/home-path/share/applications/")
+        };
+        # Note: Normally collides with the one provided with the package
 
       };
     };
