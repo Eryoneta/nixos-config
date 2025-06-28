d57812367b6a22e0defd007f7cc9664761e69b91
Eryoneta <eryoneta@gmail.com>
2025-06-28 00:19:06 -0300

    [ADD] Programs: scrcpy
    - (ADD) Controlador de dispositivos Android
    . Bom para usar o celular como uma webcam

--- users/yo/home.nix
+++ users/yo/home.nix
@@ -52,6 +52,7 @@
               extraGroups = [
                 "wheel" # Can use commands with sudo
                 "networkmanager" # Can change networking settings
+                "adbusers" # Can debug Android devices
               ];
             };
           }
