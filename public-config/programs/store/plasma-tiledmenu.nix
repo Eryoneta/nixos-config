8105e71556b78e93693be6f5386f347902a7c269
Eryoneta <eryoneta@gmail.com>
2026-05-14 21:33:47 -0300

    [ALT] Programs/Plasma/TiledMenu: mergeAttrs
    - (ALT) A função facilita o override de configurações

--- public-config/programs/store/plasma-tiledmenu.nix
+++ public-config/programs/store/plasma-tiledmenu.nix
@@ -207,18 +207,10 @@
         [ MC Ca __ __ __ __ __ ]
         [ SM __ __ Zo __ Mp Mp ]
       ]);
-      tiledmenu = (
-        let
-          base = ((config.modules."plasma-tiledmenu").attr.mkTiledmenu apps gridModel);
-        in (base // {
-          config = base.config // {
-            "General" = base.config."General" // {
-              "icon" = "nix-snowflake"; # Icon with color
-            };
-          };
-          # Note: Ugly! Ugly! Ugly! But this is necessary to not override everything, and target only the icon
-        })
-      );
+      tiledmenu = (utils.mergeAttrs ((config.modules."plasma-tiledmenu").attr.mkTiledmenu apps gridModel) {
+        config."General"."icon" = "nix-snowflake"; # Icon with color
+        config."General"."defaultTileColor" = "#F4F4F4"; # Almost white
+      });
     };
   };
 
